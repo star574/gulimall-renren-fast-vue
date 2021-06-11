de21dbb713fae9b0bdbffcd9fa32aa68060b9b87
star574 <shihengluo574@gmail.com>
2021-06-11 22:28:27 +0800

    brand

--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -14,11 +14,11 @@
       </el-form-item>
       <el-form-item>
         <el-button @click="getDataList()">查询</el-button>
-        <el-button
+        <el-buttoncen
           v-if="isAuth('product:brand:save')"
           type="primary"
           @click="addOrUpdateHandle()"
-          >新增</el-button
+          >新增</el-buttoncen
         >
         <el-button
           v-if="isAuth('product:brand:delete')"
